16685c034328da610a90b202ca49fd3fd5da10d8
hoangmai29 <mnh290905@gmail.com>
2025-07-31 00:44:46 +0700

    Update LibraryCiCdApplication.java

--- src/main/java/com/example/library/LibraryCiCdApplication.java
+++ src/main/java/com/example/library/LibraryCiCdApplication.java
@@ -1,13 +1,11 @@
 package com.example.library;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
-public class HelloController {
-
-    @GetMapping("/")
-    public String hello() {
-        return "Library App is running!";
+@SpringBootApplication
+public class LibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
     }
 }
